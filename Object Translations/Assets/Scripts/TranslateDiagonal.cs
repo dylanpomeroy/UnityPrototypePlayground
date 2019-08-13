22e3bc30b3c17619c23600582f85311c4c11b303
Dylan Pomeroy <dylan.pomeroy.j@gmail.com>
2019-08-12 23:17:13 -0400

    Finished Object Translations prototype

---
+++ Object Translations/Assets/Scripts/TranslateDiagonal.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateDiagonal : MonoBehaviour
+{
+
+    /// <summary>
+    /// The maximum global X and Y position that this GameObject can move to
+    /// </summary>
+    /// <remarks>[SerializeField] lets us see and set this value in the Unity Editor</remarks>
+    [SerializeField]
+    private Vector3 MinimumXY;
+
+    /// <summary>
+    /// The maximum global X and Y position that this GameObject can move to
+    /// </summary>
+    /// <remarks>[SerializeField] lets us see and set this value in the Unity Editor</remarks>
+    [SerializeField]
+    private Vector3 MaximumXY;
+
+    /// <summary>
+    /// The relative distance to increase/decrease the GameObject's global position by each frame
+    /// </summary>
+    /// <remarks>
+    /// [SerializeField] lets us see and set this value in the Unity Editor
+    /// </remarks>
+    [SerializeField]
+    private float StepXYDistance;
+
+    /// <summary>
+    /// The position we want the GameObject to move towards each frame.
+    /// </summary>
+    /// <remarks>We will swap this between the Maximum and Minimum values.</remarks>
+    private Vector3 goalPosition;
+
+    /// <summary>
+    /// Run one time when the scene starts.
+    /// </summary>
+    private void Start()
+    {
+        // Set our first goal Position based on the maximum position
+        goalPosition = MaximumXY;
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    private void Update()
+    {
+        // Use Vector3.Lerp to set the GameObject's position a step distance closer to the goal position
+        // - when working with multiple dimensions, it's easier to use Vector math functions like Lerp
+        transform.position = Vector3.MoveTowards(transform.position, goalPosition, StepXYDistance);
+
+        // If we are at the maximum position
+        if (transform.position == MaximumXY)
+        {
+            // Set the goal position to the minimum position
+            goalPosition = MinimumXY;
+        }
+        // If we are at the minimum position
+        else if (transform.position == MinimumXY)
+        {
+            // Set the goal position to the maximum position
+            goalPosition = MaximumXY;
+        }
+    }
+}
---
+++ Object Translations/Assets/Scripts/TranslateLeftRight.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslateLeftRight : MonoBehaviour
+{
+    /// <summary>
+    /// The maximum global X position that this GameObject can move to
+    /// </summary>
+    /// <remarks>[SerializeField] lets us see and set this value in the Unity Editor</remarks>
+    [SerializeField]
+    private float MinimumX;
+
+    /// <summary>
+    /// The maximum global X position that this GameObject can move to
+    /// </summary>
+    /// <remarks>[SerializeField] lets us see and set this value in the Unity Editor</remarks>
+    [SerializeField]
+    private float MaximumX;
+
+    /// <summary>
+    /// The relative X position to increase/decrease the GameObject's global position by each frame
+    /// </summary>
+    /// <remarks>
+    /// This value will be negated when we need to move in the opposite direction.
+    /// [SerializeField] lets us see and set this value in the Unity Editor
+    /// </remarks>
+    [SerializeField]
+    private float StepX;
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    /// <remarks>This function is special and will be automatically run by Unity</remarks>
+    private void Update()
+    {
+        // Get a vector reprsentation of our step amount
+        var stepVector = new Vector3(StepX, 0, 0);
+
+        // Add the step vector to this GameObject's transform's global position
+        transform.position += stepVector;
+
+        // If the new position is too low
+        if (transform.position.x < MinimumX)
+        {
+            // Set the position to the minimum position
+            // - This will prevent the position from ever being below the minimum height
+            transform.position = new Vector3(MinimumX, transform.position.y, transform.position.z);
+
+            // Negate the value of the step amount
+            // This will make the next frame calculate the new step in the opposite direction
+            StepX *= -1;
+        }
+        // If the new position is too high
+        else if (transform.position.x > MaximumX)
+        {
+            // Set the position to the maximum position
+            // - This will prevent the position from ever being above the maximum height
+            transform.position = new Vector3(MaximumX, transform.position.y, transform.position.z);
+
+            // Negate the value of the step amount
+            // This will make the next frame calculate the new step in the opposite direction
+            StepX *= -1;
+        }
+    }
+}
